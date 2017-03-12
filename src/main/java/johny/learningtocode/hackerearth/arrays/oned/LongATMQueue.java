18f4b25a32896975f3103f2600139f2c4ef37436
johny <johnythomas007@gmail.com>
2017-03-12 11:15:08 +0530

    Added Long ATM Queue Problem from hackerearth

---
+++ src/main/java/johny/learningtocode/hackerearth/arrays/oned/LongATMQueue.java
@@ -0,0 +1,28 @@
+package johny.learningtocode.hackerearth.arrays.oned;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * problem url: https://www.hackerearth.com/practice/data-structures/arrays/1-d/practice-problems/algorithm/long-atm-queue-3/description/
+ */
+public class LongATMQueue {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        String[] array = reader.readLine().split("\\s");
+        System.out.println(findTotalGroups(array));
+    }
+
+    static int findTotalGroups(String[] array) {
+        int totalGroups = 1;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i].length() < array[i - 1].length() || (array[i].length() == array[i - 1].length() && array[i].compareTo(array[i - 1]) < 0)) {
+                totalGroups++;
+            }
+        }
+        return totalGroups;
+    }
+}
---
+++ src/test/java/johny/learningtocode/hackerearth/arrays/oned/LongATMQueueTest.java
@@ -0,0 +1,22 @@
+package johny.learningtocode.hackerearth.arrays.oned;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LongATMQueueTest {
+
+    @Test
+    public void testLongATMQueue() {
+        String[] heightsArray = "1 2 3 4".split("\\s");
+        assertEquals(1, LongATMQueue.findTotalGroups(heightsArray));
+        heightsArray = "1 2 1 4".split("\\s");
+        assertEquals(2, LongATMQueue.findTotalGroups(heightsArray));
+        heightsArray = "1".split("\\s");
+        assertEquals(1, LongATMQueue.findTotalGroups(heightsArray));
+        heightsArray = "1 2".split("\\s");
+        assertEquals(1, LongATMQueue.findTotalGroups(heightsArray));
+        heightsArray = "1 2 1".split("\\s");
+        assertEquals(2, LongATMQueue.findTotalGroups(heightsArray));
+    }
+}
