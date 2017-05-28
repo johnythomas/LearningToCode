070e9e2f77ff83dd4f73b18e982b2110b005395e
johny <johnythomas007@gmail.com>
2017-05-28 11:47:33 +0530

    Added Memorise Me problem from hackeearth

---
+++ src/main/java/johny/learningtocode/hackerearth/ds/arrays/oned/MemoriseMe.java
@@ -0,0 +1,41 @@
+package johny.learningtocode.hackerearth.ds.arrays.oned;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Problem url: https://www.hackerearth.com/practice/data-structures/arrays/1-d/practice-problems/algorithm/memorise-me/description/
+ */
+public class MemoriseMe {
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder result = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        String[] array = reader.readLine().split("\\s");
+        Map<Integer, Integer> numberFrequency = findNumberFrequency(array, n);
+        int q = Integer.parseInt(reader.readLine());
+        while (q-- > 0) {
+            int frequency = executeQuery(Integer.parseInt(reader.readLine()), numberFrequency);
+            result.append(frequency == 0 ? "NOT PRESENT" : frequency).append("\n");
+        }
+        System.out.println(result);
+    }
+
+    static Map<Integer, Integer> findNumberFrequency(String[] array, int n) {
+        Map<Integer, Integer> numberFrequency = new HashMap<>();
+        for (int index = 0; index < n; index++) {
+            int element = Integer.parseInt(array[index]);
+            numberFrequency.putIfAbsent(element, 0);
+            numberFrequency.put(element, numberFrequency.get(element) + 1);
+        }
+        return numberFrequency;
+    }
+
+    static int executeQuery(int b, Map<Integer, Integer> numberFrequency) {
+        return numberFrequency.get(b) == null ? 0 : numberFrequency.get(b);
+    }
+}
---
+++ src/test/java/johny/learningtocode/hackerearth/ds/arrays/oned/MemoriseMeTest.java
@@ -0,0 +1,27 @@
+package johny.learningtocode.hackerearth.ds.arrays.oned;
+
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+
+public class MemoriseMeTest {
+
+    @Test
+    public void testMemorizeMe() {
+        String[] array = "1 2 3 4".split("\\s");
+        Map<Integer, Integer> numberFrequency = MemoriseMe.findNumberFrequency(array, 4);
+        numberFrequency.values().stream().forEach(val -> assertEquals(1, val.intValue()));
+        Stream.of(array).forEach(ele -> assertEquals(1, MemoriseMe.executeQuery(Integer.parseInt(ele), numberFrequency)));
+        assertEquals(0, MemoriseMe.executeQuery(7, numberFrequency));
+
+        array = "1 2 3 4 1".split("\\s");
+        Map<Integer, Integer> numberFrequency1 = MemoriseMe.findNumberFrequency(array, 5);
+        assertEquals(2, MemoriseMe.executeQuery(1, numberFrequency1));
+        assertEquals(1, MemoriseMe.executeQuery(2, numberFrequency1));
+        assertEquals(0, MemoriseMe.executeQuery(5, numberFrequency1));
+    }
+
+}
