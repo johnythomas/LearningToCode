5f8bdb820ac281a817e5dc145d5dea27e8d44e4d
johny <johnythomas007@gmail.com>
2017-08-26 17:11:44 +0530

    Added Insert a node at the head of a linked list from hackerrank

---
+++ js/hackerrank/datastructures/LinkedLists/InsertNodeAtHeadLinkedList.js
@@ -0,0 +1,20 @@
+// Problem url: https://www.hackerrank.com/challenges/insert-a-node-at-the-head-of-a-linked-list
+'use strict';
+
+/*
+  Insert Node at the beginning of a linked list
+  head pointer input could be NULL as well for empty list
+  Node is defined as
+  var Node = function(data) {
+      this.data = data;
+      this.next = null;
+  }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+function insert(head, data) {
+  let newNode = new Node(data);
+  
+}
